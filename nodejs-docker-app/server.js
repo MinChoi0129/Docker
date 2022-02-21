991a565bdbba2278cad0b4b3ad0b84148fd9aa19
MinJae Lee <wjdchs0129@gnu.ac.kr>
2022-02-21 23:51:58 +0900

    docker node js app

---
+++ nodejs-docker-app/server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const PORT = 8080
+const app = express()
+app.get('/', (req, res) => {
+    res.send("반갑습니다.")
+})
+
+app.listen(PORT, () => {
+    console.log(`포트 ${PORT}번에서 실행 중..`)
+})ㄴ
