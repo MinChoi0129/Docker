9dc8168b15ef5c6d1d4c03b7ab5ef66397ffcfb1
MinJae Lee <wjdchs0129@gnu.ac.kr>
2022-02-22 22:50:34 +0900

    docker file update with node js

--- nodejs-docker-app/server.js
+++ nodejs-docker-app/server.js
@@ -2,9 +2,9 @@ const express = require('express')
 const PORT = 8080
 const app = express()
 app.get('/', (req, res) => {
-    res.send("반갑습니다.")
+    res.send("new 안녕하세요.")
 })
 
 app.listen(PORT, () => {
     console.log(`포트 ${PORT}번에서 실행 중..`)
-})ㄴ
+})
